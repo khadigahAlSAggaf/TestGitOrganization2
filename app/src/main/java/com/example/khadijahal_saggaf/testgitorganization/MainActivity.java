b57457eb36cc79be3ce855556a64dbe2ee5e7338
KhadijahAl_Saggaf <ksakkaf@stu.kau.edu.sa>
2018-11-01 13:07:25 +0300

    Initial commit

---
+++ app/src/main/java/com/example/khadijahal_saggaf/testgitorganization/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.khadijahal_saggaf.testgitorganization;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
